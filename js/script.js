70faa8e6fd4b9bbbf10daa30d36de2965213ffc9
Maria Siapera <mariasiapera@outlook.com>
2017-03-18 20:38:10 +0200

    Fixed api key

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@
 function initMap() {
 	var location = {lat:37.980519, lng:23.754072};
 	var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 4,
+          zoom: 15,
           center: location
         });
 	var marker = new google.maps.Marker({
